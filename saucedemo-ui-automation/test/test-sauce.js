1fbc3a96bdb5714a1ef88ebb4e29ddcb167e1db9
aghniafyaa <aghniafyak01@gmail.com>
2026-06-25 15:25:10 +0700

    Add Selenium SauceDemo automation for sesi 9

---
+++ saucedemo-ui-automation/test/test-sauce.js
@@ -0,0 +1,76 @@
+const { Builder, By, until } = require('selenium-webdriver')
+const assert = require('assert');
+
+describe('Sauce Demo Test', function(){
+
+    it ('Sukses Login', async function(){
+        let driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get('https://www.saucedemo.com');
+
+        await driver.findElement(By.css('[data-test="username"]')).sendKeys('standard_user');
+        await driver.findElement(By.css('[data-test="password"]')).sendKeys('secret_sauce');
+
+        await driver.findElement(By.css('[data-test="login-button"]')).click();
+
+        let appLogo = await driver.wait(
+            until.elementLocated(By.className('app_logo')),
+            10000
+        );
+        let logoText = await appLogo.getText();
+
+        assert.strictEqual(logoText, 'Swag Labs');
+
+        console.log('Login berhasil');
+
+        await driver.sleep(2000);
+        await driver.quit();
+
+    });
+    it('Urutkan Produk dari A-Z', async function () {
+
+        let driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get('https://www.saucedemo.com');
+
+        await driver.findElement(By.css('[data-test="username"]'))
+            .sendKeys('standard_user');
+
+        await driver.findElement(By.css('[data-test="password"]'))
+            .sendKeys('secret_sauce');
+
+        await driver.findElement(By.css('[data-test="login-button"]'))
+            .click();
+
+        await driver.wait(
+            until.elementLocated(By.className('app_logo')),
+            10000
+        );    
+
+        let sortDropdown = await driver.findElement(
+            By.css('[data-test="product-sort-container"]')
+        );
+
+        await sortDropdown.click();
+
+        await sortDropdown.findElement(
+            By.css('option[value="az"]')
+        ).click();
+
+        let firstProduct = await driver.findElement(
+            By.css('.inventory_item_name')
+        );
+
+        let firstProductName = await firstProduct.getText();
+
+        assert.strictEqual(
+            firstProductName,
+            'Sauce Labs Backpack'
+        );
+
+        console.log('Sort A-Z berhasil');
+
+        await driver.sleep(2000);
+        await driver.quit();
+    });
+})
